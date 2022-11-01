3c535d888b3dd213c72224a00db713efea2c7c87
s.mahmoodi <sepidemahmoodi@badbadak.ir>
2022-11-01 09:46:27 +0330

    first steps for learning python until arthmetic uperator

---
+++ app.py
@@ -0,0 +1,46 @@
+
+# print('sepide')
+
+# name = input('what is your name? ')
+# favorite_color = input('and whats your favorite color? ');
+#
+# print(name + ' likes ' + favorite_color)
+
+
+# birth_year = input('whats your birth year? ')
+# age = 2022 - int(birth_year)
+# print(age)
+
+# weight = input('whats your weight in pounds ? ')
+# kg_weight = round(int(weight) / 2.205)
+# print(kg_weight)
+
+# course = "python's course for beginers" #if i had apostroph we should use double coutation
+
+# example = '''
+# hi john
+# this is our first email for you
+# '''
+# print(example)
+
+
+# course = 'Python for beginers'
+# print(course[0:3])
+# print(course[1:])
+# print(course[:5])
+# print(course[:]) # all characters
+#
+# name = 'jennifer'
+# print(name[1:-1]) #ennife
+
+
+# first = 'sepide'
+# last = 'smith'
+# msg = f'{first} [{last}] is a coder'
+# print(msg)
+
+# name = 'jennifer'
+# print(len(name))
+
+# str = 'python is the best'
+# print('python' in str)
